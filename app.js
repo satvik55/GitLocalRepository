ff03d789adc85bf5c5f0d3b675cff728874478ff
satvik55 <satvikbodke@gmail.com>
2024-07-31 13:47:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import the HTTP module
+const http = require('http');
+
+// Define the port number
+const PORT = 3000;
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Send the response body
+  res.end('Hello, World!\n');
+});
+
+// Start the server and listen on the specified port
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
